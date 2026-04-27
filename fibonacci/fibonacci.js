06711ddaaa1cfb0ce6a9d7d5eda838adb04b4080
Anna-Gladush <ann.gladush@gmail.com>
2026-04-27 23:49:09 +0200

    figuring out fibonacci sequence recursion

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,13 +1,31 @@
-const fibonacci = (n) => {
-  if (n <= 1) { 
-    return [0, 1]
+let res = []
+const fibonacci = (n, prev = 0, next = 1) => {
+  if (n < 1) { 
+    return res
   } else {
-    // generate fibonacci sequence up to n - 1
-    const seq = fibonacci(n - 1);
-    // calculate next term in the series
-    seq.push(seq[seq.length - 1] + seq[seq.length - 2]) 
-  return seq.slice(0, n)
-
+    res.push(prev)
+    fibonacci(n - 1, next, prev + next)
   }
+  return res
 }
+
+
+// not recursive
+// let fibonacci_not = (n) => {
+//   let results = [];
+//   let prev = 0;
+//   let next = 1;
+//   let fib;
+//   for (let i = 0; i < n; i++) {
+//     if (i == 0 || i == 1) {
+//       results.push(i);
+//     } else {
+//       fib = prev + next
+//       prev = next;
+//       next = fib
+//       results.push(fib)
+//     }
+//   }
+//   return results.slice(0, n)
+// }
 export default fibonacci
