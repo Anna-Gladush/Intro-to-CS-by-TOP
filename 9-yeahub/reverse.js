0c896710bf908151035fd07403ec31e89fe66485
Anna-Gladush <ann.gladush@gmail.com>
2026-09-18 15:55:26 +0200

    task: solved reverse.js

---
+++ 9-yeahub/reverse.js
@@ -0,0 +1,9 @@
+// Обращение строки или массива
+
+function reverse(input) {
+  if (typeof input === "string") return input.split("").reverse().join("");
+  if (Array.isArray(input)) return input.reverse();
+  return input;
+}
+
+console.log(reverse([5, 4, 3, 2, 1]));
